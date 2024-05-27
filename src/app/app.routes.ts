411cf13522e19b9a4da04ad8d468a8f032dca7d0
Luis Gomero <lgomero94@gmail.com>
2024-05-26 21:33:59 -0500

    update components - Prettier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { LabsComponent } from './pages/labs/labs.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'labs',
-        component: LabsComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'labs',
+    component: LabsComponent,
+  },
 ];
